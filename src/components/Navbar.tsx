d3a71f7991a327cb2d71cfa4daf3df0431f4dc18
Kenny Howard <khoward@beyond.com>
2024-09-24 11:39:00 -0600

    moving navbar styles out of global styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,31 +2,26 @@ import Link from 'next/link';
 import Logo from './Logo';
 import ThemeToggle from './ThemeToggle';
 
+// styles
+import styles from './Navbar.module.scss';
+
 const Navbar = () => {
     return (
         <nav>
-            <div className="nav-container">
+            <div className={styles.navContainer}>
                 <Logo />
                 <ul>
                     <li>
-                        <Link href="/">
-                            HOME
-                        </Link>
+                        <Link href="/">HOME</Link>
                     </li>
                     <li>
-                        <Link href="/matches">
-                            MATCHES
-                        </Link>
+                        <Link href="/matches">MATCHES</Link>
                     </li>
                     <li>
-                        <Link href="/table">
-                            TABLE
-                        </Link>
+                        <Link href="/table">TABLE</Link>
                     </li>
                     <li>
-                        <Link href="/stats">
-                            STATS
-                        </Link>
+                        <Link href="/stats">STATS</Link>
                     </li>
                 </ul>
                 <ThemeToggle />
