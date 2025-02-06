87dfe3f1a78cb6b7696fc87541745a4fa53c9bd0
Kenny Howard <ken.chad.howard@gmail.com>
2025-02-05 18:40:23 -0700

    boop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 import { useGlobal } from '../context/GlobalContext';
 // components
 import Navbar from './Navbar';
-import MobileNavbar from './MobileNavbar';
+import MobileNavbar from './MobileNavBar';
 
 const Layout = ({ children }: { children: ReactNode }) => {
     const { isMobile } = useGlobal();
